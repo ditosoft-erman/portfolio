c33ffa8b25d32f228534c405facc066a5cdf4af4
Erman Louie Faminiano <ermanfaminiao020@gmail.com>
2024-03-18 08:53:13 +0800

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}",
+  "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {
+      animation: {
+        "loop-scroll": "loop-scroll 60s linear infinite ",
+      },
+      keyframes:{
+        "loop-scroll":{
+        from: {transform: "translateX(0)"},
+        to: {transform: "translateX(-100%)"},
+        },
+      },
       colors: {
         'meow': '#17191A',
       },
       backgroundImage: {
-        'bg-intro' : "url('src/assets/image/png/bground.svg')",
-        'bg-fineworks' : "url('src/assets/image/png/fine-bg.png')",
+        'primary': "url('../src/assets/image/png/bround.png')",
+      'fine': "url('../src/assets/image/png/fine-works.png')",
+        'bg-intro' : "url('src/assets/image/png/bround.png')",
         'bg-eat-sleep-repeat' :"url('src/assets/image/png/eat-sleep-edit-repeat.png')",
       }
     },
@@ -17,6 +28,8 @@ module.exports = {
       mont: ['Montserrat', 'sans-serif'],
     },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
 
